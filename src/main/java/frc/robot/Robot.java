18d985b08bc28c59810b853ac44ef47a3fc942e1
Kshitij Teotia <teotia.kshitij@gmail.com>
2020-02-20 10:52:20 -0800

    Gyro Turn

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.GyroTurn;
 import frc.robot.commands.JoystickDrive;
 import frc.robot.subsystems.DriveTrain;
 
@@ -98,7 +99,7 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.cancel();
     }
 
-    new JoystickDrive(robotContainer.drive).schedule();
+    new GyroTurn(robotContainer.drive).schedule();
   }
 
   /**
